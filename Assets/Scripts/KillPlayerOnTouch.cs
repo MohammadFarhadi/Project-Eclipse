0e6dcfe6a9a1345643786d3a1b2783e8add8f3f7
Amir13-11-1383 <156079474+Amir13-11-1383@users.noreply.github.com>
2025-05-15 11:42:07 -0700

    Spike , Tramboline ,Gear wheel complete

---
+++ Assets/Scripts/KillPlayerOnTouch.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class KillPlayerOnTouch : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Devrease Health of Player");
+        }
+    }
+}
