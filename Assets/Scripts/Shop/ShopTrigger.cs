7a3967fc8c48f4e3bcc17b2aec0584e58b7a9264
MohammadFarhadi <88154623+MohammadFarhadi@users.noreply.github.com>
2025-06-01 15:20:35 +0330

    log system (not added)

--- Assets/Scripts/Shop/ShopTrigger.cs
+++ Assets/Scripts/Shop/ShopTrigger.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class ShopTrigger : MonoBehaviour
 {
     public GameObject shopUI;
-    public ShopManager shopManager;  // ارجاع به اسکریپت ShopManager
+    public ShopManager shopManager;  // ارجاع به اسکریپت ShopManager]
+    PlayerControllerBase currentPlayer;
+
 
     private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
+    {        
+        PlayerControllerBase player1 = other.GetComponent<PlayerControllerBase>();
+        if (player1 != null)
+        {
+            currentPlayer = player1;
+        }
+        
+        if (other.CompareTag("Player") && currentPlayer.IsInteracting())
         {
             shopUI.SetActive(true);
             var player = other.GetComponent<PlayerControllerBase>();
@@ -17,7 +25,7 @@ public class ShopTrigger : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") || currentPlayer.IsInteracting())
         {
             shopUI.SetActive(false);
             shopManager.SetCurrentPlayer(null);
