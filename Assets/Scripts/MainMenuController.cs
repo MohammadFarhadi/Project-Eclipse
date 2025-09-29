28a7bf32c9a995247168295f6d08a60ccb0bbb0c
Amir13-11-1383 <156079474+Amir13-11-1383@users.noreply.github.com>
2025-09-28 21:36:59 -0700

    Ui Updated

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("پنل‌ها")]
+    public GameObject mainMenuPanel;    // پنل اصلی
+    public GameObject onlinePanel;      // پنل Online Mode
+    public GameObject localPanel;       // پنل Local Mode
+    public GameObject optionPanel;      // پنل Option
+    public GameObject creditPanel;      // پنل Credit
+
+    private GameObject currentPanel;    // پنل فعلی که نمایش داده می‌شود
+
+    // نمایش پنل Online
+    public void OnOnlineModeClicked()
+    {
+        SwitchPanel(onlinePanel);
+    }
+
+    // نمایش پنل Local
+    public void OnLocalModeClicked()
+    {
+        SwitchPanel(localPanel);
+    }
+
+    // نمایش پنل Option
+    public void OnOptionClicked()
+    {
+        SwitchPanel(optionPanel);
+    }
+
+    // نمایش پنل Credit
+    public void OnCreditClicked()
+    {
+        SwitchPanel(creditPanel);
+    }
+
+    // خروج از برنامه
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // برای تست داخل Editor
+#endif
+    }
+
+    // متد بازگشت به منوی اصلی
+    public void OnBackClicked()
+    {
+        if (currentPanel != null)
+        {
+            currentPanel.GetComponent<UIAnimator>().Hide(); // فقط پنل فعلی بسته شود
+            currentPanel = null;
+        }
+
+        mainMenuPanel.GetComponent<UIAnimator>().Show(); // نمایش منوی اصلی
+    }
+
+    // متد کمکی برای تغییر پنل‌ها
+    private void SwitchPanel(GameObject targetPanel)
+    {
+        mainMenuPanel.GetComponent<UIAnimator>().Hide();
+
+        if (currentPanel != null)
+        {
+            currentPanel.GetComponent<UIAnimator>().Hide();
+        }
+
+        targetPanel.GetComponent<UIAnimator>().Show();
+        currentPanel = targetPanel;
+    }
+}
